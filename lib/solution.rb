b270584acf7510d4ff3a75f1218d7644a8931169
vibraniumforge <gregm9999@gmail.com>
2019-09-10 21:59:39 -0700

    Done.

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,3 +1,12 @@
 def clock_angle(time)
-    # code your solution here
+	hours = time.split(":")[0].to_i
+	minutes = time.split(":")[1].to_i
+	way1 = (360 - ((hours * 30) + (minutes * 0.5)) - (minutes * 6)).abs
+	way2 = (0 + ((hours * 30) + (minutes * 0.5)) - ((minutes * 6))).abs
+	[way1, way2].min
+
+	#  9:00 => 90
+	#  3:15 => 7.5
+	#  8:30 => 75
+
 end
